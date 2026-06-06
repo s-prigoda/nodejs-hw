c4230ec6a0f72292075cb082d5fb710b1216965c
Prigoda <sergey.prigoda@gmail.com>
2026-06-06 16:29:17 +0300

    fix hw4

--- src/models/session.js
+++ src/models/session.js
@@ -4,7 +4,7 @@ const sessionSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, required: true },
     accessToken: { type: String, required: true },
-    refreshToten: { type: String, required: true },
+    refreshToken: { type: String, required: true },
     accessTokenValidUntil: { type: Date, required: true },
     refreshTokenValidUntil: { type: Date, required: true },
   },
